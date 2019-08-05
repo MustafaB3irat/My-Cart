5000d4ac5fe9a76d08df05ca91f03ba119029594
MustafaB3irat <mustafaadwi@gmail.com>
2019-08-05 15:12:02 +0300

    updated

--- app/src/main/java/com/example/icart/views/SplashScreenActivity.java
+++ app/src/main/java/com/example/icart/views/SplashScreenActivity.java
@@ -7,11 +7,13 @@ import android.os.Handler;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.icart.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static final int SPLASH_SCREEN_TIMEOUT = 2000;
+    private static final int SPLASH_SCREEN_TIMEOUT = 2500;
 
 
     @Override
@@ -21,10 +23,21 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         new Handler().postDelayed(() -> {
 
+            YoYo.with(Techniques.FadeIn).duration(500).playOn(findViewById(R.id.r));
+            YoYo.with(Techniques.Bounce).duration(700).playOn(findViewById(R.id.r));
+
+        }, 1000);
+
+        new Handler().postDelayed(() -> {
+
             Intent intent = new Intent(this.getBaseContext(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(intent);
+            overridePendingTransition(0, 0);
             finish();
 
         }, SPLASH_SCREEN_TIMEOUT);
     }
+
+
 }
